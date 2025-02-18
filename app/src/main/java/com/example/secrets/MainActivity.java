1a5ad3935e565699b4b748736e3b878fbb7b9b2d
ceo1 <christian4onos@outlook.com>
2025-02-18 19:24:16 +0100

    more editing

--- app/src/main/java/com/example/secrets/MainActivity.java
+++ app/src/main/java/com/example/secrets/MainActivity.java
@@ -9,7 +9,6 @@ import android.provider.MediaStore;
 import android.util.Log;
 import android.widget.ImageView;
 import android.widget.ImageButton;
-import android.widget.ImageView;
 import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
 import androidx.activity.result.ActivityResultLauncher;
