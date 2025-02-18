24ce80e2593513c57182921efc8eeed7af49dc08
ceo1 <christian4onos@outlook.com>
2025-02-18 16:41:43 +0100

    color syncing II

--- app/src/main/java/com/example/secrets/MainActivity.java
+++ app/src/main/java/com/example/secrets/MainActivity.java
@@ -7,8 +7,6 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.Toast;
